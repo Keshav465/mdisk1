321d052b24d27643641b66a2f0e6844b8638b7c0
keshav6606 <cinemahub77@gmail.com>
2025-10-12 10:46:17 +0530

    Update groups.py

--- bot/database/groups.py
+++ bot/database/groups.py
@@ -1,66 +1,165 @@
-from datetime import datetime
-import time
-from bot.config import Config
-from motor.motor_asyncio import AsyncIOMotorClient
+import asyncio
+import re
+from pyrogram import Client, filters, types as t, enums
+from bot.config import Config, Script
+from bs4 import BeautifulSoup
+from bot.utils import auto_delete_func, filter_chat, create_telegraph_post, is_premium_group, short_from_text, remove_link, remove_mention
+from bot.database import group_db
 
 
-class Database:
-    def __init__(self, uri, database_name):
-        self._client = AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.groups = self.db["groups"]
 
-    async def get_group(self, group_id):
-        group = await self.groups.find_one({"group_id": int(group_id)})
-        if not group:
-            res = {
-                "group_id": group_id,
-                "banned": False,
-                "shortener_site":None,
-                "shortener_api":None,
-                'fsub': True,
-                'fsub_channel': 0,
-                'index_channel':0,
-                "last_verified": datetime(1970,1,1),
-                "access_days":0, 
-                "auto_delete":True, 
-                "auto_delete_time":300, 
-            }
-            await self.groups.insert_one(res)
-            group = await self.groups.find_one({"group_id": group_id})
-        return group
-
-    async def update_group(self, group_id, value):
-        myquery = {
-            "group_id": group_id,
-        }
-        newvalues = {"$set": value}
-        return await self.groups.update_one(myquery, newvalues)
-
-    async def filter_groups(self):
-        return self.groups.find({})
-
-    async def total_groups_count(self, ):
-        return await self.groups.count_documents({})
-
-    async def get_all_groups(self, ):
-        return self.groups.find({})
-
-    async def delete_group(self, group_id):
-        await self.groups.delete_one({"group_id": int(group_id)})
-
-    async def is_group_exist(self, id):
-        group = await self.groups.find_one({"group_id": int(id)})
-        return bool(group)
-
-    async def is_group_verified(self, group_id):
-        group = await self.get_group(group_id)
-        access_days = datetime.fromtimestamp(time.mktime(group["last_verified"].timetuple()) + group['access_days'])
-        return (access_days - datetime.now()).total_seconds() >= 0
-
-    async def expiry_date(self, group_id):
-        group = await self.get_group(group_id)
-        access_days = datetime.fromtimestamp(time.mktime(group["last_verified"].timetuple()) + group['access_days'])
-        return access_days, int((access_days - datetime.now()).total_seconds())
+@Client.on_message(filters.text & (filters.private | filters.group) & filters.incoming)
+async def pm_filter(c, m: t.Message):
     
-group_db = Database(Config.DATABASE_URL, Config.DATABASE_NAME)
+    free_group = True
+    if m.chat.type in [enums.chat_type.ChatType.SUPERGROUP, enums.chat_type.ChatType.GROUP]:
+        chat_id = getattr(m.chat, "id", None)
+        if await is_premium_group(chat_id):
+            free_group = False
+
+    query = m.text
+
+    if m.text.startswith("/"):
+        return  # ignore commands
+    if re.findall(r"((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", query):
+        return
+
+    if 2 < len(query) < 100:
+
+        is_private = m.chat.type == enums.chat_type.ChatType.PRIVATE
+
+        database_channels, auto_delete, auto_delete_time, shortener_api, shortener_site, force_sub_channel, force_sub = None, None, None, None, None, None, None
+        if m.chat.type == enums.chat_type.ChatType.PRIVATE or free_group:
+            database_channels = Config.DATABASE_CHANNEL
+            auto_delete = Config.AUTO_DELETE
+            auto_delete_time = Config.AUTO_DELETE_TIME
+            shortener_api = Config.SHORTENER_API
+            shortener_site = Config.SHORTENER_SITE
+        elif m.chat.type in [enums.chat_type.ChatType.SUPERGROUP, enums.chat_type.ChatType.GROUP]:
+            grp_id = m.chat.id
+            group_info = await group_db.get_group(grp_id)
+            database_channels = [group_info["index_channel"]
+                                 ] if group_info["index_channel"] else []
+            auto_delete = group_info["auto_delete"]
+            auto_delete_time = group_info["auto_delete_time"]
+            
+            shortener_api = group_info["shortener_api"]
+            shortener_site = group_info["shortener_site"]
+
+
+        is_shortener = bool(shortener_api and shortener_site)
+        
+        if is_shortener:
+            database_channels = Config.DATABASE_CHANNEL
+
+        if not database_channels:
+            return
+
+        sts = await m.reply("`Searching...`")
+
+        try:
+            results = await filter_chat(c, query, database_channels)
+        except Exception:
+            await sts.edit("Some error occured")
+            return
+
+        template = "<aside><b>{i}.{title}</b><br><a href='{link}'>ðŸ‘‰ Click Here To Download</a> | {id}</aside><hr>"
+        bin_text = ""
+        i = 1
+        for result in results:
+            result: t.Message
+
+            text_ = result.text or result.caption
+            title = text_.splitlines()[0]
+            link = 0
+            if free_group or is_shortener:
+                bot_username = c.username.replace("@", "")
+                link_temp = f"https://telegram.dog/{bot_username}?start=file_"
+                link = None
+                if result.document or result.video:
+                    title = remove_mention(remove_link(title))
+                    link =  f"{link_temp}{result.id}_{result.chat.id}"
+                
+            elif result.photo or result.text:
+                link = result.link
+
+            temp = template.format(
+                i=i,
+                title=title,
+                link=link,
+                id=result.id
+            ) if link else None
+
+            if text_ := temp:
+                bin_text += text_
+                i += 1
+
+        if not bin_text:
+            await not_found_response(sts, query)
+            return
+
+        if is_shortener:
+            bin_text = await short_from_text(shortener_api, shortener_site, bin_text)
+
+        text = f"<h3>Results for {query}</h3><br><h4>Total results: {i-1}</h4><br><hr>{bin_text}"
+
+        soup = BeautifulSoup(text, "html.parser")
+        formatted_text = soup.prettify()
+
+        reply_url = await create_telegraph_post(query, formatted_text)
+
+        reply_markup = t.InlineKeyboardMarkup(
+            [
+                [
+                    t.InlineKeyboardButton(
+                        "How to Download?",
+                        url=Config.RESULTS_HOW_TO_DOWNLOAD_LINK,
+                    ),
+                ],
+                [
+                    t.InlineKeyboardButton(
+                        "Request Movie",
+                        url=Config.REQUEST_MOVIE_URL,
+                    )
+                ],
+            ]
+        ) if Config.RESULTS_HOW_TO_DOWNLOAD_LINK and Config.REQUEST_MOVIE_URL and is_private else None
+
+        replied_link = await sts.edit(Script.RESULTS_MESSAGE.format(
+            query=query.upper(),
+            url=reply_url
+        ), disable_web_page_preview=1,
+        reply_markup=reply_markup 
+        )
+
+        if bool(auto_delete and auto_delete_time):
+            asyncio.create_task(auto_delete_func(
+                replied_link, auto_delete_time))
+
+        return
+
+
+async def not_found_response(m, query):
+    reply = query.replace(" ", "+")
+    reply_markup = t.InlineKeyboardMarkup(
+        [
+            [
+                t.InlineKeyboardButton(
+                    "Check Release Date",
+                    url=f"https://www.google.com/search?q={reply}+movie+release+date",
+                ),
+            ],
+            [
+                t.InlineKeyboardButton(
+                    "ðŸ” Click to Check Spellingâœ…",
+                    url=f"https://www.google.com/search?q={reply}+movie",
+                )
+            ],
+        ]
+    )
+
+    return await m.edit(
+        Script.NO_REPLY_TEXT.format(query),
+        disable_web_page_preview=0,
+        reply_markup=reply_markup,
+            )
