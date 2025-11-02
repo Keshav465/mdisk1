44d8dabe1d70c70d0e8d62b1b8dd857700bd1c12
keshav6606 <cinemahub77@gmail.com>
2025-11-02 12:16:37 +0530

    Update group_filter.py

--- bot/plugins/group_filter.py
+++ bot/plugins/group_filter.py
@@ -3,13 +3,20 @@ import re
 from pyrogram import Client, filters, types as t, enums
 from bot.config import Config, Script
 from bs4 import BeautifulSoup
-from bot.utils import auto_delete_func, filter_chat, create_telegraph_post, is_premium_group, short_from_text, remove_link, remove_mention
+from bot.utils import (
+    auto_delete_func,
+    filter_chat,
+    create_telegraph_post,
+    is_premium_group,
+    short_from_text,
+    remove_link,
+    remove_mention
+)
 from bot.database import group_db
 
 
 @Client.on_message(filters.text & (filters.private | filters.group) & filters.incoming)
 async def pm_filter(c, m: t.Message):
-    
     free_group = True
     if m.chat.type in [enums.chat_type.ChatType.SUPERGROUP, enums.chat_type.ChatType.GROUP]:
         chat_id = getattr(m.chat, "id", None)
@@ -24,10 +31,9 @@ async def pm_filter(c, m: t.Message):
         return
 
     if 2 < len(query) < 100:
-
         is_private = m.chat.type == enums.chat_type.ChatType.PRIVATE
 
-        database_channels, auto_delete, auto_delete_time, shortener_api, shortener_site, force_sub_channel, force_sub = None, None, None, None, None, None, None
+        database_channels, auto_delete, auto_delete_time, shortener_api, shortener_site = None, None, None, None, None
         if m.chat.type == enums.chat_type.ChatType.PRIVATE or free_group:
             database_channels = Config.DATABASE_CHANNEL
             auto_delete = Config.AUTO_DELETE
@@ -44,7 +50,7 @@ async def pm_filter(c, m: t.Message):
             shortener_site = group_info["shortener_site"]
 
         is_shortener = bool(shortener_api and shortener_site)
-        
+
         if is_shortener:
             database_channels = Config.DATABASE_CHANNEL
 
@@ -56,7 +62,7 @@ async def pm_filter(c, m: t.Message):
         try:
             results = await filter_chat(c, query, database_channels)
         except Exception:
-            await sts.edit("Some error occured")
+            await sts.edit("Some error occurred")
             return
 
         template = "<aside><b>{i}. {title}</b><br><a href='{link}'>👉 Click Here To Download</a> | {id}</aside><hr>"
@@ -67,27 +73,19 @@ async def pm_filter(c, m: t.Message):
 
             text_ = result.text or result.caption
             title = text_.splitlines()[0]
-            link = 0
+            link = None
             if free_group or is_shortener:
                 bot_username = c.username.replace("@", "")
                 link_temp = f"https://telegram.dog/{bot_username}?start=file_"
-                link = None
                 if result.document or result.video:
                     title = remove_mention(remove_link(title))
                     link = f"{link_temp}{result.id}_{result.chat.id}"
-                
             elif result.photo or result.text:
                 link = result.link
 
-            temp = template.format(
-                i=i,
-                title=title,
-                link=link,
-                id=result.id
-            ) if link else None
-
-            if text_ := temp:
-                bin_text += text_
+            if link:
+                temp = template.format(i=i, title=title, link=link, id=result.id)
+                bin_text += temp
                 i += 1
 
         if not bin_text:
@@ -98,113 +96,36 @@ async def pm_filter(c, m: t.Message):
             bin_text = await short_from_text(shortener_api, shortener_site, bin_text)
 
         text = f"<h3>Results for {query}</h3><br><h4>Total results: {i-1}</h4><br><hr>{bin_text}"
-
         soup = BeautifulSoup(text, "html.parser")
         formatted_text = soup.prettify()
-
         reply_url = await create_telegraph_post(query, formatted_text)
 
-        reply_markup = t.InlineKeyboardMarkup(
-            [
+        reply_markup = None
+        if Config.RESULTS_HOW_TO_DOWNLOAD_LINK and Config.REQUEST_MOVIE_URL and is_private:
+            reply_markup = t.InlineKeyboardMarkup([
                 [
                     t.InlineKeyboardButton(
-                        "How to Download?",
+                        "🎬 How to Download?",
                         url=Config.RESULTS_HOW_TO_DOWNLOAD_LINK,
                     ),
                 ],
                 [
                     t.InlineKeyboardButton(
-                        "Request Movie",
+                        "🎥 Request Movie",
                         url=Config.REQUEST_MOVIE_URL,
                     )
                 ],
-                [
-                    t.InlineKeyboardButton(
-                        "🎬 Visit Our Movie Site",
-                        url="https://filmy4uhd.vercel.app",
-                    )
-                ]
-            ]
-        ) if Config.RESULTS_HOW_TO_DOWNLOAD_LINK and Config.REQUEST_MOVIE_URL and is_private else None
-
-        # 💡 Added website link in result message
-        site_url = "https://filmy4uhd.vercel.app"
-        result_text = f"""
-{Script.RESULTS_MESSAGE.format(query=query.upper(), url=reply_url)}
-
-🔗 <b>Visit our website for more movies:</b> <a href='{site_url}'>{site_url}</a>
-"""
+            ])
 
         replied_link = await sts.edit(
-            result_text,
-            disable_web_page_preview=0,
+            Script.RESULTS_MESSAGE.format(query=query.upper(), url=reply_url),
+            disable_web_page_preview=True,
             reply_markup=reply_markup
         )
 
         if bool(auto_delete and auto_delete_time):
             asyncio.create_task(auto_delete_func(replied_link, auto_delete_time))
 
-        return
-
-
-# 🔹 Modified Not Found Response (with site link)
-async def not_found_response(m, query):
-    reply = query.replace(" ", "+")
-    site_url = "https://filmy4uhd.vercel.app"
-
-    reply_markup = t.InlineKeyboardMarkup(
-        [
-            [
-                t.InlineKeyboardButton(
-                    "Check Release Date",
-                    url=f"https://www.google.com/search?q={reply}+movie+release+date",
-                ),
-            ],
-            [
-                t.InlineKeyboardButton(
-                    "🔍 Click to Check Spelling✅",
-                    url=f"https://www.google.com/search?q={reply}+movie",
-                )
-            ],
-            [
-                t.InlineKeyboardButton(
-                    "🎬 Visit Our Movie Site",
-                    url=site_url,
-                )
-            ]
-        ]
-    )
-
-    return await m.edit(
-        f"{Script.NO_REPLY_TEXT.format(query)}\n\n<b>Also visit our website:</b> <a href='{site_url}'>{site_url}</a>",
-        disable_web_page_preview=0,
-        reply_markup=reply_markup,
-    )
-                        url=Config.RESULTS_HOW_TO_DOWNLOAD_LINK,
-                    ),
-                ],
-                [
-                    t.InlineKeyboardButton(
-                        "Request Movie",
-                        url=Config.REQUEST_MOVIE_URL,
-                    )
-                ],
-            ]
-        ) if Config.RESULTS_HOW_TO_DOWNLOAD_LINK and Config.REQUEST_MOVIE_URL and is_private else None
-
-        replied_link = await sts.edit(Script.RESULTS_MESSAGE.format(
-            query=query.upper(),
-            url=reply_url
-        ), disable_web_page_preview=1,
-        reply_markup=reply_markup 
-        )
-
-        if bool(auto_delete and auto_delete_time):
-            asyncio.create_task(auto_delete_func(
-                replied_link, auto_delete_time))
-
-        return
-
 
 async def not_found_response(m, query):
     reply = query.replace(" ", "+")
@@ -212,21 +133,27 @@ async def not_found_response(m, query):
         [
             [
                 t.InlineKeyboardButton(
-                    "Check Release Date",
+                    "📅 Check Release Date",
                     url=f"https://www.google.com/search?q={reply}+movie+release+date",
                 ),
             ],
             [
                 t.InlineKeyboardButton(
-                    "🔍 Click to Check Spelling✅",
+                    "🔍 Check Spelling",
                     url=f"https://www.google.com/search?q={reply}+movie",
-                )
+                ),
+            ],
+            [
+                t.InlineKeyboardButton(
+                    "🎬 Visit Filmy4uHD",
+                    url="https://filmy4uhd.vercel.app",
+                ),
             ],
         ]
     )
 
     return await m.edit(
         Script.NO_REPLY_TEXT.format(query),
-        disable_web_page_preview=0,
+        disable_web_page_preview=False,
         reply_markup=reply_markup,
     )
